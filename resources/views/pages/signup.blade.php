915dbb69886e00550351a92a3217b439de186e23
Dissdiw <adilitc.05@gmail.com>
2024-01-16 21:39:41 +0700

    create  StudentController

---
+++ resources/views/pages/signup.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <div class="text-red">
+        signup
+    </div>
+    @push('css')
+    <style>
+        .text-red{
+            color : red;
+        }
+    </style>
+    @endpush
+</x-app-layout>
